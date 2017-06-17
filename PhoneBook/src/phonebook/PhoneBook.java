703e7cee11bfed840843051b37ef8d47b66ad6be
mhcrnl <mhcrnl@gmail.com>
2017-06-17 10:59:24 +0300

    17.06.2017

---
+++ PhoneBook/src/phonebook/PhoneBook.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonebook;
+
+/**
+ *
+ * @author mhcrnl
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PhoneBook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Salut din Agenda Telefonica!");
+        
+        String csvFile = "contacts.csv";
+        String line = "";
+        String csvSplitBy = ",";
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))){
+            while((line = br.readLine()) != null){
+                //utilizarea virgulei ca separator
+                String[] country = line.split(csvSplitBy);
+                System.out.println("Nume:  "+country[1]+" "+country[2]);
+                System.out.println("Nr.Tel:  "+country[3]+" Email: "+country[4]);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # PhoneBook
-Creating a phonebook
-Using contacts.csv file and create phonebook.
+Creating a phonebook in NetBeans 8.1 and reading from contacts.csv file and print phonebook.
 
 ## Instalare
 ```
 git clone https://github.com/mhcrnl/PhoneBook-29.git
 ```
 ## Compilare si executare
+
+Deschiderea proiectului in NetBeans IDE.
+
 ``` bash
 javac PhoneBook.java    //compileaza aplicatia
 java PhoneBook          //executa aplicatia
